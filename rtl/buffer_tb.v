35e865b20e5b7cb897335f001fb0eb695a2d4ada
Narcis Rodas <narcis.rodas.quiroga@gmail.com>
2020-03-30 16:03:38 +0200

    updated buffer submodule test-bench

--- rtl/buffer_tb.v
+++ rtl/buffer_tb.v
@@ -11,12 +11,13 @@ module buffer_tb;
     reg [6:0] din;  
     wire [6:0] dout;
     reg write_done;
+    reg ini_read_done;
     reg error;
 
     initial begin
         $dumpfile("buffer_tb.vcd");
         $dumpvars(0, buffer_tb);
-        wait(row_r == 5'd29 && col_r == 7'd79); // last tile, bottom right
+        wait(row_r == 5'd29 && col_r == 7'd79 && write_done); // last tile, bottom right
         if (error == 1'b0) $display("PASS");
         $finish;
     end
@@ -30,6 +31,7 @@ module buffer_tb;
         din = 7'd0;
         wr_en = 1'b1;
         write_done = 1'b0;
+        ini_read_done = 1'b0;
         error = 1'b0;
     end
         
@@ -37,27 +39,45 @@ module buffer_tb;
 
     // this test writes every tile starting from bottom right with increasing numbers starting from 0 and then reads all the tiles starting from top left
     always @(posedge clk) begin
-        #1 if (write_done) begin
-            if (dout != din) begin
+        #1 if (!ini_read_done) begin
+            if (dout != 7'd0) begin
                 $display("ERROR at column %d row %d dout %d din %d", col_r, row_r, dout, din);
                 error <= 1'b1;
             end
-            din <= din - 1;
             col_r <= col_r + 1;
             if (col_r == 7'd79) begin
                 col_r <= 7'd0;
                 row_r <= row_r + 1;
             end
-        end
-        if (col_w != 7'd0 || row_w != 5'd0) begin
-            din <= din + 1;
-            col_w <= col_w - 1;
-            if (col_w == 7'd0) begin
-                col_w <= 7'd79;
-                row_w <= row_w - 1;
+            if (row_r == 5'd29 && col_r == 7'd79) begin
+                ini_read_done <= 1'b1;
+                row_r <= 5'd0;
+                col_r <= 7'd0;
             end
         end
-        else write_done <= 1'b1;
+        else begin
+            if (write_done) begin
+                if (dout != din) begin
+                    $display("ERROR at column %d row %d dout %d din %d", col_r, row_r, dout, din);
+                    error <= 1'b1;
+                end
+                din <= din - 1;
+                col_r <= col_r + 1;
+                if (col_r == 7'd79) begin
+                    col_r <= 7'd0;
+                    row_r <= row_r + 1;
+                end
+            end
+            if (col_w != 7'd0 || row_w != 5'd0) begin
+                din <= din + 1;
+                col_w <= col_w - 1;
+                if (col_w == 7'd0) begin
+                    col_w <= 7'd79;
+                    row_w <= row_w - 1;
+                end
+            end
+            else write_done <= 1'b1;
+        end
     end 
 
     /* Make a regular pulsing clock. */
